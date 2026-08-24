b3d7b04f97942ef7bdfc770935bbf5342a8ef707
kunalkdm1442005-gif <kunalkdm1442005@gmail.com>
2026-08-24 22:47:17 +0530

    Add files via upload

---
+++ supabase/functions/_shared/catalogue.ts
@@ -0,0 +1,641 @@
+// Generated from catalogue-data.js. Re-generate this file whenever catalogue prices change.
+export type CatalogueProduct = {
+  name: string;
+  category: string;
+  pack: string;
+  price: number;
+  image: string;
+};
+
+export const catalogueById = new Map<string, CatalogueProduct>([
+  [
+    "heimac-labs-whey-protein-1-kg-1",
+    {
+      "name": "Heimac Labs Whey Protein",
+      "category": "Whey Protein",
+      "pack": "1 kg",
+      "price": 2895,
+      "image": "catalogue-images/catalogue-01.jpg"
+    }
+  ],
+  [
+    "muscletech-nitro-tech-1-72-kg-2",
+    {
+      "name": "MuscleTech Nitro-Tech",
+      "category": "Whey Protein",
+      "pack": "1.72 kg",
+      "price": 2499,
+      "image": "catalogue-images/catalogue-02.jpg"
+    }
+  ],
+  [
+    "optimum-nutrition-gold-standard-100-whey-2-27-kg-3",
+    {
+      "name": "Optimum Nutrition Gold Standard 100% Whey",
+      "category": "Whey Protein",
+      "pack": "2.27 kg",
+      "price": 9800,
+      "image": "catalogue-images/catalogue-03.jpg"
+    }
+  ],
+  [
+    "newgen-labs-generation-whey-2-265-kg-4",
+    {
+      "name": "Newgen Labs Generation Whey",
+      "category": "Whey Protein",
+      "pack": "2.265 kg",
+      "price": 8800,
+      "image": "catalogue-images/catalogue-04.jpg"
+    }
+  ],
+  [
+    "omaxgen-whey-2-kg-5",
+    {
+      "name": "Omaxgen Whey",
+      "category": "Whey Protein",
+      "pack": "2 kg",
+      "price": 8800,
+      "image": "catalogue-images/catalogue-05.jpg"
+    }
+  ],
+  [
+    "muscletiet-whey-protein-1kg-6",
+    {
+      "name": "MuscleTiet Whey Protein",
+      "category": "Whey Protein",
+      "pack": "1kg",
+      "price": 2800,
+      "image": "catalogue-images/catalogue-06.jpg"
+    }
+  ],
+  [
+    "rule-1-whey-protein-2-28kg-7",
+    {
+      "name": "Rule 1 Whey Protein",
+      "category": "Whey Protein",
+      "pack": "2.28kg",
+      "price": 8800,
+      "image": "catalogue-images/catalogue-07.jpg"
+    }
+  ],
+  [
+    "flex-wheeler-whey-protein-2kg-8",
+    {
+      "name": "Flex Wheeler Whey Protein",
+      "category": "Whey Protein",
+      "pack": "2kg",
+      "price": 9800,
+      "image": "catalogue-images/catalogue-08.jpg"
+    }
+  ],
+  [
+    "nutricore-clean-whey-2-kg-9",
+    {
+      "name": "Nutricore Clean Whey",
+      "category": "Whey Protein",
+      "pack": "2 kg",
+      "price": 7800,
+      "image": "catalogue-images/catalogue-09.jpg"
+    }
+  ],
+  [
+    "ultimate-nutrition-prostar-100-whey-2-9-kg-5-28-lb-2-39-kg-10",
+    {
+      "name": "Ultimate Nutrition Prostar 100% Whey",
+      "category": "Whey Protein",
+      "pack": "2.9 kg 5.28 lb / 2.39 kg",
+      "price": 9000,
+      "image": "catalogue-images/catalogue-10.jpg"
+    }
+  ],
+  [
+    "bsn-syntha-6-2-27-kg-11",
+    {
+      "name": "BSN Syntha-6",
+      "category": "Whey Protein",
+      "pack": "2.27 kg",
+      "price": 6600,
+      "image": "catalogue-images/catalogue-11.jpg"
+    }
+  ],
+  [
+    "essential-series-2kg-12",
+    {
+      "name": "Essential Series",
+      "category": "Whey Protein",
+      "pack": "2kg",
+      "price": 6000,
+      "image": "catalogue-images/catalogue-12.jpg"
+    }
+  ],
+  [
+    "muscletiet-bobx-whey-gold-100-whey-2-kg-13",
+    {
+      "name": "MuscleTiet BOBX Whey Gold 100% Whey",
+      "category": "Whey Protein",
+      "pack": "2 kg",
+      "price": 6400,
+      "image": "catalogue-images/catalogue-13.jpg"
+    }
+  ],
+  [
+    "muscletiet-whey-challenge-2-kg-14",
+    {
+      "name": "MuscleTiet Whey Challenge",
+      "category": "Whey Protein",
+      "pack": "2 kg",
+      "price": 6400,
+      "image": "catalogue-images/catalogue-14.jpg"
+    }
+  ],
+  [
+    "avvatar-whey-protein-1-kg-1-kg-15",
+    {
+      "name": "Avvatar Whey Protein - 1 kg",
+      "category": "Whey Protein",
+      "pack": "1 kg",
+      "price": 4200,
+      "image": "catalogue-images/catalogue-15.jpg"
+    }
+  ],
+  [
+    "apex-vitals-protein-blend-1-kg-16",
+    {
+      "name": "Apex Vitals Protein Blend",
+      "category": "Whey Protein",
+      "pack": "1 kg",
+      "price": 4000,
+      "image": "catalogue-images/catalogue-16.jpg"
+    }
+  ],
+  [
+    "muscleblaze-biozyme-iso-zero-1-kg-17",
+    {
+      "name": "MuscleBlaze Biozyme Iso-Zero",
+      "category": "Whey Protein",
+      "pack": "1 kg",
+      "price": 3599,
+      "image": "catalogue-images/catalogue-17.jpg"
+    }
+  ],
+  [
+    "myogenetix-whey-protein-concentrate-2-kg-18",
+    {
+      "name": "Myogenetix Whey Protein Concentrate",
+      "category": "Whey Protein",
+      "pack": "2 kg",
+      "price": 7000,
+      "image": "catalogue-images/catalogue-18.jpg"
+    }
+  ],
+  [
+    "fitness-peanut-butter-variant-1-1-kg-19",
+    {
+      "name": "Fitness Peanut Butter - Variant 1",
+      "category": "Peanut Butter",
+      "pack": "1 kg",
+      "price": 799,
+      "image": "catalogue-images/catalogue-19.jpg"
+    }
+  ],
+  [
+    "fitness-peanut-butter-variant-2-see-pack-20",
+    {
+      "name": "Fitness Peanut Butter - Variant 2",
+      "category": "Peanut Butter",
+      "pack": "See pack",
+      "price": 650,
+      "image": "catalogue-images/catalogue-19.jpg"
+    }
+  ],
+  [
+    "dymatize-iso100-fruity-pebbles-2-kg-21",
+    {
+      "name": "Dymatize ISO100 Fruity Pebbles",
+      "category": "Whey Isolate",
+      "pack": "2 kg",
+      "price": 17500,
+      "image": "catalogue-images/catalogue-20.jpg"
+    }
+  ],
+  [
+    "dymatize-iso100-cinnamon-churro-2-3-kg-22",
+    {
+      "name": "Dymatize ISO100 Cinnamon Churro",
+      "category": "Whey Isolate",
+      "pack": "2.3 kg",
+      "price": 17500,
+      "image": "catalogue-images/catalogue-21.jpg"
+    }
+  ],
+  [
+    "isopure-whey-protein-isolate-1-kg-1-kg-23",
+    {
+      "name": "Isopure Whey Protein Isolate - 1 kg",
+      "category": "Whey Isolate",
+      "pack": "1 kg",
+      "price": 7000,
+      "image": "catalogue-images/catalogue-22.png"
+    }
+  ],
+  [
+    "isopure-whey-protein-isolate-2-kg-2-kg-24",
+    {
+      "name": "Isopure Whey Protein Isolate - 2 kg",
+      "category": "Whey Isolate",
+      "pack": "2 kg",
+      "price": 13300,
+      "image": "catalogue-images/catalogue-23.jpg"
+    }
+  ],
+  [
+    "one-science-iso-gold-2kg-25",
+    {
+      "name": "One Science ISO Gold",
+      "category": "Whey Isolate",
+      "pack": "2kg",
+      "price": 10500,
+      "image": "catalogue-images/catalogue-24.jpg"
+    }
+  ],
+  [
+    "the-whole-truth-whey-protein-1-kg-26",
+    {
+      "name": "The Whole Truth Whey Protein",
+      "category": "Whey Isolate",
+      "pack": "1  kg",
+      "price": 4400,
+      "image": "catalogue-images/catalogue-25.jpg"
+    }
+  ],
+  [
+    "truegain-mass-gainer-1-7-kg-27",
+    {
+      "name": "TrueGain Mass Gainer",
+      "category": "Mass Gainers",
+      "pack": "1.7 kg",
+      "price": 4600,
+      "image": "catalogue-images/catalogue-26.jpg"
+    }
+  ],
+  [
+    "muscletiet-muscle-mass-gainer-10-lbs-28",
+    {
+      "name": "MuscleTiet Muscle Mass Gainer",
+      "category": "Mass Gainers",
+      "pack": "10 LBS",
+      "price": 6800,
+      "image": "catalogue-images/catalogue-27.jpg"
+    }
+  ],
+  [
+    "essential-series-muscle-builder-5-kg-29",
+    {
+      "name": "Essential Series Muscle Builder",
+      "category": "Mass Gainers",
+      "pack": "5 kg",
+      "price": 4800,
+      "image": "catalogue-images/catalogue-28.jpg"
+    }
+  ],
+  [
+    "optimum-nutrition-serious-mass-3-kg-30",
+    {
+      "name": "Optimum Nutrition Serious Mass",
+      "category": "Mass Gainers",
+      "pack": "3 kg",
+      "price": 4099,
+      "image": "catalogue-images/catalogue-29.jpg"
+    }
+  ],
+  [
+    "heimac-labs-mass-gainer-3-5-kg-31",
+    {
+      "name": "Heimac Labs Mass Gainer",
+      "category": "Mass Gainers",
+      "pack": "3.5 kg",
+      "price": 5000,
+      "image": "catalogue-images/catalogue-30.jpg"
+    }
+  ],
+  [
+    "wrathx-pre-workout-540-g-32",
+    {
+      "name": "WRATHX Pre-Workout",
+      "category": "Pre-Workout",
+      "pack": "540 g",
+      "price": 2200,
+      "image": "catalogue-images/catalogue-31.png"
+    }
+  ],
+  [
+    "bsn-no-xplode-555-g-33",
+    {
+      "name": "BSN NO-XPLODE",
+      "category": "Pre-Workout",
+      "pack": "555 g",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-32.png"
+    }
+  ],
+  [
+    "redcon1-total-war-420-g-34",
+    {
+      "name": "REDCON1 Total War",
+      "category": "Pre-Workout",
+      "pack": "420 g",
+      "price": 4099,
+      "image": "catalogue-images/catalogue-33.png"
+    }
+  ],
+  [
+    "nitraflex-300-2-g-35",
+    {
+      "name": "NitraFlex",
+      "category": "Pre-Workout",
+      "pack": "300.2 g",
+      "price": 3200,
+      "image": "catalogue-images/catalogue-34.png"
+    }
+  ],
+  [
+    "nitraflex-309-g-36",
+    {
+      "name": "NitraFlex",
+      "category": "Pre-Workout",
+      "pack": "309 g",
+      "price": 3200,
+      "image": "catalogue-images/catalogue-35.jpg"
+    }
+  ],
+  [
+    "body-fuel-pre-workout-320-g-37",
+    {
+      "name": "Body Fuel Pre-Workout",
+      "category": "Pre-Workout",
+      "pack": "320 g",
+      "price": 2999,
+      "image": "catalogue-images/catalogue-36.png"
+    }
+  ],
+  [
+    "insane-labz-psychotic-185-g-38",
+    {
+      "name": "Insane Labz Psychotic",
+      "category": "Pre-Workout",
+      "pack": "185 g",
+      "price": 2800,
+      "image": "catalogue-images/catalogue-37.png"
+    }
+  ],
+  [
+    "cellucor-c4-original-390-g-39",
+    {
+      "name": "Cellucor C4 Original",
+      "category": "Pre-Workout",
+      "pack": "390 g",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-38.png"
+    }
+  ],
+  [
+    "flex-wheeler-boom-pre-workout-300-g-40",
+    {
+      "name": "Flex Wheeler Boom Pre-Workout",
+      "category": "Pre-Workout",
+      "pack": "300 g",
+      "price": 2400,
+      "image": "catalogue-images/catalogue-39.png"
+    }
+  ],
+  [
+    "heimac-labs-pure-pump-449-g-41",
+    {
+      "name": "Heimac Labs Pure Pump",
+      "category": "Pre-Workout",
+      "pack": "449 g",
+      "price": 3500,
+      "image": "catalogue-images/catalogue-40.png"
+    }
+  ],
+  [
+    "muscletiet-platinum-creatine-250-g-42",
+    {
+      "name": "MuscleTiet Platinum Creatine",
+      "category": "Creatine / Amino Acids",
+      "pack": "250 g",
+      "price": 1800,
+      "image": "catalogue-images/catalogue-41.png"
+    }
+  ],
+  [
+    "muscletiet-glutamine-250-g-43",
+    {
+      "name": "MuscleTiet Glutamine",
+      "category": "Creatine / Amino Acids",
+      "pack": "250 g",
+      "price": 1800,
+      "image": "catalogue-images/catalogue-42.png"
+    }
+  ],
+  [
+    "black-series-turbo-250-g-44",
+    {
+      "name": "Black series Turbo",
+      "category": "Creatine / Amino Acids",
+      "pack": "250 g",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-43.jpg"
+    }
+  ],
+  [
+    "xtend-original-bcaa-390-g-45",
+    {
+      "name": "XTEND Original BCAA",
+      "category": "BCAA / EAA",
+      "pack": "390 g",
+      "price": 2800,
+      "image": "catalogue-images/catalogue-44.png"
+    }
+  ],
+  [
+    "black-series-creatine-250-g-46",
+    {
+      "name": "Black Series Creatine",
+      "category": "Creatine / Amino Acids",
+      "pack": "250 g",
+      "price": 2500,
+      "image": "catalogue-images/catalogue-45.png"
+    }
+  ],
+  [
+    "muscleblaze-creatine-monohydrate-320-g-47",
+    {
+      "name": "MuscleBlaze Creatine Monohydrate",
+      "category": "Creatine / Amino Acids",
+      "pack": "320 g",
+      "price": 1200,
+      "image": "catalogue-images/catalogue-46.png"
+    }
+  ],
+  [
+    "qnt-creatine-monohydrate-80-g-48",
+    {
+      "name": "QNT Creatine Monohydrate",
+      "category": "Creatine / Amino Acids",
+      "pack": "80 g",
+      "price": 699,
+      "image": "catalogue-images/catalogue-47.png"
+    }
+  ],
+  [
+    "wellcore-micronised-creatine-monohydrate-122-g-122-g-33-servings-49",
+    {
+      "name": "Wellcore Micronised Creatine Monohydrate - 122 g",
+      "category": "Creatine / Amino Acids",
+      "pack": "122 g (33 servings)",
+      "price": 699,
+      "image": "catalogue-images/catalogue-48.png"
+    }
+  ],
+  [
+    "wellcore-micronised-creatine-monohydrate-250-g-250-g-83-servings-50",
+    {
+      "name": "Wellcore Micronised Creatine Monohydrate - 250 g",
+      "category": "Creatine / Amino Acids",
+      "pack": "250 g (83 servings)",
+      "price": 1399,
+      "image": "catalogue-images/catalogue-49.jpg"
+    }
+  ],
+  [
+    "muscletiet-multivitamin-100-g-51",
+    {
+      "name": "MuscleTiet Multivitamin",
+      "category": "Vitamins & Minerals",
+      "pack": "100 g",
+      "price": 1000,
+      "image": "catalogue-images/catalogue-50.png"
+    }
+  ],
+  [
+    "v-shape-supps-vitamin-complex-120-tablets-52",
+    {
+      "name": "V Shape Supps Vitamin Complex",
+      "category": "Vitamins & Minerals",
+      "pack": "120 tablets",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-51.png"
+    }
+  ],
+  [
+    "v-shape-omega-supplement-100-tablets-53",
+    {
+      "name": "V Shape Omega Supplement",
+      "category": "Vitamins & Minerals",
+      "pack": "100 tablets",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-52.png"
+    }
+  ],
+  [
+    "pro-level-labs-alpha-see-pack-54",
+    {
+      "name": "Pro Level Labs Alpha",
+      "category": "Anabolic Supplements",
+      "pack": "See pack",
+      "price": 7500,
+      "image": "catalogue-images/catalogue-53.png"
+    }
+  ],
+  [
+    "testrol-gold-tablets-60-tablets-visible-55",
+    {
+      "name": "Testrol Gold Tablets",
+      "category": "Anabolic Supplements",
+      "pack": "60 tablets visible",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-54.png"
+    }
+  ],
+  [
+    "myogenetix-myodrol-see-pack-56",
+    {
+      "name": "Myogenetix Myodrol",
+      "category": "Anabolic Supplements",
+      "pack": "See pack",
+      "price": 4500,
+      "image": "catalogue-images/catalogue-55.png"
+    }
+  ],
+  [
+    "ghrp6-sigmadrol-product-see-pack-57",
+    {
+      "name": "GHRP6 / SigmaDrol Product",
+      "category": "Anabolic Supplements",
+      "pack": "See pack",
+      "price": 3800,
+      "image": "catalogue-images/catalogue-56.png"
+    }
+  ],
+  [
+    "organjet-pro-tablets-58",
+    {
+      "name": "Organjet Pro",
+      "category": "Health & Wellness",
+      "pack": "Tablets",
+      "price": 3000,
+      "image": "catalogue-images/catalogue-57.png"
+    }
+  ],
+  [
+    "v-shape-citrulline-300-g-59",
+    {
+      "name": "V Shape Citrulline",
+      "category": "Amino Acids",
+      "pack": "300 g",
+      "price": 2499,
+      "image": "catalogue-images/catalogue-58.png"
+    }
+  ],
+  [
+    "gnc-calcium-plus-60-tablets-visible-60",
+    {
+      "name": "GNC Calcium Plus",
+      "category": "Vitamins & Minerals",
+      "pack": "60 tablets visible",
+      "price": 399,
+      "image": "catalogue-images/catalogue-59.png"
+    }
+  ],
+  [
+    "black-mamba-90-capsule-61",
+    {
+      "name": "Black Mamba",
+      "category": "Health & Wellness",
+      "pack": "90 capsule",
+      "price": 2800,
+      "image": "catalogue-images/catalogue-60.png"
+    }
+  ],
+  [
+    "killer-bees-60-capsule-62",
+    {
+      "name": "Killer Bees",
+      "category": "Health & Wellness",
+      "pack": "60 capsule",
+      "price": 3000,
+      "image": "catalogue-images/catalogue-61.png"
+    }
+  ],
+  [
+    "vk-nutrition-shaker-bottle-shaker-bottle-63",
+    {
+      "name": "VK Nutrition Shaker Bottle",
+      "category": "Gym Accessories",
+      "pack": "Shaker bottle",
+      "price": 299,
+      "image": "catalogue-images/catalogue-62.png"
+    }
+  ]
+]);
